9d2f8379c7289bf0fa30a0bef7baf5927905af01
武双 <wushshsha@163.com>
2019-02-28 15:36:46 +0800

    1，调整位置，emotions

--- README.md
+++ README.md
@@ -27,3 +27,11 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+# 修改源码，添加emotions 对class的支持
+                    if (src) {
+                        // 加一个 data-w-e 属性，点击图片的时候不再提示编辑图片
+                        faceHtml += '<span class="w-e-item"><img src="' + src + '" alt="' + alt + '" class="' + _class + '" data-w-e="1"/></span>';
+                    }
+
--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -58,6 +58,360 @@ export default {
       //'undo',  // 撤销
       //'redo'  // 重复
     ]; /**/
+
+    // 表情面板可以有多个 tab ，因此要配置成一个数组。数组每个元素代表一个 tab 的配置
+    this.editor.customConfig.emotions = [
+      {
+        // tab 的标题
+        title: "Default",
+        // type -> 'emoji' / 'image'
+        type: "image",
+        // content -> 数组
+        content: [
+          {
+            alt: "aini",
+            src: "/normal/2018new_aini_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "baibai_thumb.png",
+            src: "/normal/2018new_baibai_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "beishang",
+            src: "/normal/2018new_beishang_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "bingbujiandan_thumb.png",
+            src: "/normal/2018new_bingbujiandan_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "bishi",
+            src: "/normal/2018new_bishi_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "bizui",
+            src: "/normal/2018new_bizui_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "chanzui",
+            src: "/normal/2018new_chanzui_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "chigua_thumb.png",
+            src: "/normal/2018new_chigua_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "chijing",
+            src: "/normal/2018new_chijing_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "chongjing",
+            src: "/normal/2018new_chongjing_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "dahaqian",
+            src: "/normal/2018new_dahaqian_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "dalian",
+            src: "/normal/2018new_dalian_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "ding",
+            src: "/normal/2018new_ding_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "guzhang_thumb.png",
+            src: "/normal/2018new_guzhang_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "haha_thumb.png",
+            src: "/normal/2018new_haha_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "haixiu",
+            src: "/normal/2018new_haixiu_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "han",
+            src: "/normal/2018new_han_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "heixian_thumb.png",
+            src: "/normal/2018new_heixian_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "heng_thumb.png",
+            src: "/normal/2018new_heng_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "huaixiao",
+            src: "/normal/2018new_huaixiao_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "huaxin",
+            src: "/normal/2018new_huaxin_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "jiyan",
+            src: "/normal/2018new_jiyan_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "keai",
+            src: "/normal/2018new_keai_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "kelian",
+            src: "/normal/2018new_kelian_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "kouzhao_thumb.png",
+            src: "/normal/2018new_kouzhao_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "ku",
+            src: "/normal/2018new_ku_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "kun_thumb.png",
+            src: "/normal/2018new_kun_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "kuxiao",
+            src: "/normal/2018new_kuxiao_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "landelini",
+            src: "/normal/2018new_landelini_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "leimu",
+            src: "/normal/2018new_leimu_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "ningwen",
+            src: "/normal/2018new_ningwen_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "nu_thumb.png",
+            src: "/normal/2018new_nu_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "qian_thumb.png",
+            src: "/normal/2018new_qian_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "qinqin_thumb.png",
+            src: "/normal/2018new_qinqin_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "shayan",
+            src: "/normal/2018new_shayan_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "shengbing_thumb.png",
+            src: "/normal/2018new_shengbing_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "shuai_thumb.png",
+            src: "/normal/2018new_shuai_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "shuijiao_thumb.png",
+            src: "/normal/2018new_shuijiao_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "sikao",
+            src: "/normal/2018new_sikao_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "taikaixin",
+            src: "/normal/2018new_taikaixin_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "tianping_thumb.png",
+            src: "/normal/2018new_tianping_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "touxiao",
+            src: "/normal/2018new_touxiao_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "tu",
+            src: "/normal/2018new_tu_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "wabi_thumb.png",
+            src: "/normal/2018new_wabi_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "weiqu_thumb.png",
+            src: "/normal/2018new_weiqu_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "weixioa02",
+            src: "/normal/2018new_weixioa02_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "wenhao_thumb.png",
+            src: "/normal/2018new_wenhao_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "wu_thumb.png",
+            src: "/normal/2018new_wu_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "xiaoerbuyu",
+            src: "/normal/2018new_xiaoerbuyu_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "xiaoku_thumb.png",
+            src: "/normal/2018new_xiaoku_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "xixi_thumb.png",
+            src: "/normal/2018new_xixi_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "xu",
+            src: "/normal/2018new_xu_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "yinxian",
+            src: "/normal/2018new_yinxian_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "youhengheng_thumb.png",
+            src: "/normal/2018new_youhengheng_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "yun_thumb.png",
+            src: "/normal/2018new_yun_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "zhouma_thumb.png",
+            src: "/normal/2018new_zhouma_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "zhuakuang",
+            src: "/normal/2018new_zhuakuang_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "zuohengheng_thumb.png",
+            src: "/normal/2018new_zuohengheng_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "zuoyi",
+            src: "/normal/2018new_zuoyi_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "good",
+            src: "/normal/2018new_good_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "guolai_thumb.png",
+            src: "/normal/2018new_guolai_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "jiayou",
+            src: "/normal/2018new_jiayou_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "no",
+            src: "/normal/2018new_no_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "ok",
+            src: "/normal/2018new_ok_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "ruo",
+            src: "/normal/2018new_ruo_org.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "woshou_thumb.png",
+            src: "/normal/2018new_woshou_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "ye_thumb.png",
+            src: "/normal/2018new_ye_thumb.png",
+            class: "img-normal-class"
+          },
+          {
+            alt: "zan",
+            src: "/normal/2018new_zan_org.png",
+            class: "img-normal-class"
+          }
+        ]
+      }
+    ];
+
     this.editor.customConfig.zIndex = 500;
     this.editor.create();
   }
@@ -67,5 +421,9 @@ export default {
 <style>
 .w-e-text-container {
   height: 50px !important;
+}
+img.img-normal-class {
+  width: 22px;
+  height: 22px;
 }
 </style>
